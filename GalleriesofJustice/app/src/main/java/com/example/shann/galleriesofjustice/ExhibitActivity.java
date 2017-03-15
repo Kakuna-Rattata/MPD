24fcb2058368ed179e89386ba0fc30a75cf9939a
Kakuna-Rattata <purplefuse@gmail.com>
2017-03-15 14:14:20 +0000

    Added new beacon range for Beetroot beacon, Added MainActivity as ExhibitActiviy parents

--- GalleriesofJustice/app/src/main/java/com/example/shann/galleriesofjustice/ExhibitActivity.java
+++ GalleriesofJustice/app/src/main/java/com/example/shann/galleriesofjustice/ExhibitActivity.java
@@ -1,13 +1,48 @@
 package com.example.shann.galleriesofjustice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class ExhibitActivity extends AppCompatActivity {
+
+    private static final String TAG = "Lifecycle: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exhibit_activity);
+
+        Log.d(TAG, "onCreate() called");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
--- GalleriesofJustice/app/src/main/java/com/example/shann/galleriesofjustice/MyApplication.java
+++ GalleriesofJustice/app/src/main/java/com/example/shann/galleriesofjustice/MyApplication.java
@@ -44,10 +44,17 @@ public class MyApplication extends Application {
             "Lemon beacon",
             UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
             28651, 37405);  // iBeacon format Major, minor values to identify particular beacon
+    final  Region regionBeetroot = new Region(
+            "Beetroot beacon",
+            UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+            18129, 1432);
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        final Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
         /*  Firebase database setup : */
         if(!FirebaseApp.getApps(this).isEmpty()) {
@@ -65,6 +72,7 @@ public class MyApplication extends Application {
             public void onServiceReady() {
                 beaconManager.startMonitoring(regionAll);
                 beaconManager.startMonitoring(regionLemon);
+                beaconManager.startMonitoring(regionBeetroot);
             }
         });
 
@@ -72,23 +80,49 @@ public class MyApplication extends Application {
             @Override
             public void onEnteredRegion(Region region, List<Beacon> list) {
 
+                Log.d("monitoring: enter", region.toString());
+                Log.d("region identifier", region.getIdentifier());
+
+                //  Get beacon's MajorMinor key
+                String beaconKey = String.format("%d:%d", region.getMajor(), region.getMinor());
+
+//              Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+//              mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
                 if (region == regionAll) {
                     showNotification(
                             "Welcome to The Galleries of Justice Museum",       // Title
                             "Come inside and try our new interactive tour!",    // Message
-                            MainActivity.class                                  // Open on tap
+                            mainIntent
+                            //MainActivity.class                                  // Open on tap
                     );
-
-                    Log.d("monitoring: enter", region.toString());
                 }
-                else if (region == regionLemon) {
-                    showNotification(
-                            "Convict Ship",
-                            "Check out this exhibit",
-                            ExhibitActivity.class
-                    );
+                else {
 
-                    Log.d("monitoring: enter", region.toString());
+                    //  Trigger Exhibit Activity, provide region's MM key
+                    Intent exhibitIntent = new Intent(getApplicationContext(), ExhibitActivity.class);
+                    exhibitIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    exhibitIntent.putExtra("beaconKey", beaconKey);
+                    startActivity(exhibitIntent);
+
+                    if (region == regionLemon) {
+
+                        showNotification(
+                                "Prison Cells",
+                                "Check out this exhibit",
+                                exhibitIntent
+                                //ExhibitActivity.class
+                        );
+                    } else if (region == regionBeetroot) {
+
+                        showNotification(
+                                "Courtroom",
+                                "Check out this exhibit",
+                                exhibitIntent
+                                //ExhibitActivity.class
+                        );
+
+                    }
                 }
             }
 
@@ -97,10 +131,11 @@ public class MyApplication extends Application {
 
                 if (region == regionAll) {
                     showNotification(
-                            "Thank you for shopping with us",
-                            "Check back soon for the latest " +
-                                    "app instore discounts",
-                            MainActivity.class
+                            "Thanks for visiting!",
+                            "Feel free to leave Feedback",
+                            mainIntent
+                            //MainActivity.class
+                            //TODO: Feedback activity
                     );
 
                     Log.d("monitoring: exit", region.toString());
@@ -111,9 +146,10 @@ public class MyApplication extends Application {
 
     /* Add a notification to show up whenever
      * user enters the range of our monitored beacon. */
-    public void showNotification(String title, String message, Class intentActivityClass) {
+    public void showNotification(String title, String message, Intent notificationIntent) {
+        //Class intentActivityClass
 
-        Intent notificationIntent = new Intent(this, intentActivityClass);
+        //Intent notificationIntent = new Intent(this, intentActivityClass);
         notificationIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivities(this, 0, new Intent[] {notificationIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
 
